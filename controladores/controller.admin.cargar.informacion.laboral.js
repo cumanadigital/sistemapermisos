d6593c9b747b22c3aff683a3132c16984b6abfd4
Oswaldo Hernandez <oswaldoehc@gmail.com>
2017-10-20 23:09:53 -0400

    Agregados vista y controlador para Formulario Cargar Archivo de Informacion Laboral Formato CSV

---
+++ controladores/controller.admin.cargar.informacion.laboral.js
@@ -0,0 +1,48 @@
+    console.log("javascript - root informacion laboral");
+
+    //var API_URL_periodos =  "servicios/services.periodos.php?accion=consultar_periodos";
+    // var API_URL_periodos =  "servicios/services.admin.periodos.php?accion=consultar_periodos";
+    // var API_URL_info_laboral =  "servicios/services.informacion.laboral.php?accion=consultar_informacion";
+    // var API_URL =  "servicios/services.admin.periodos.php";
+    // var $table = $('#table').bootstrapTable({url: API_URL_periodos});
+    // var $table = $('#table').bootstrapTable({url: API_URL_info_laboral});
+
+    
+    // $modal = $('#modal_registros').modal({show: false});
+    
+    $alert = $('.alert').hide();
+    
+    var accion="";
+ 
+    $(function () {
+        // create event
+    });
+    //toolbar1
+
+  
+    // function UnirFormatter(value, row) {
+    //     // console.log(row);
+    //     return ['Habitación: ' + row.telf_hab, '<br />Célular: ' + row.telf_movil].join(' ');
+    // }
+    // function UnirFormatter2(value, row) {
+    //     // console.log(row); 
+    //     return ['Email: ' + row.email, '<br />Twitter: ' + row.twitter].join(' ');
+    // }
+
+    // function UnirFormatter3(value, row) {
+    //     // console.log(row); 
+    //     return ('Nóminal:<br /><b>' + row.cod_dependencia_cobro + '</b><br />' + 'Laboral:<br /><b>' + row.cod_dependencia_labora + '</b><br />' + 'Horas: <b>' + row.horas) + '</b>';
+    // }
+    // function UnirFormatter4(value, row) {
+    //     // console.log(row);
+    //     return ('Alergias: ' + row.alergias + '<br />Cirugias: ' + row.cirugias + '\n G. Sang: ' + row.grupo_sanguineo);
+    // }
+
+  
+    // function showAlert(title, type) {
+    //     $alert.attr('class', 'alert alert-' + type || 'success')
+    //           .html('<i class="glyphicon glyphicon-check"></i> ' + title).show();
+    //     setTimeout(function () {
+    //         $alert.hide();
+    //     }, 3000);
+    // }
---
+++ vistas/vista_admin_cargar_informacion_laboral.php
@@ -0,0 +1,98 @@
+<?php 
+require_once('../conf/config.php');
+require_once('../apiv3.0/funciones/funciones3.0.php');
+?>
+
+<div class="row">
+  <div class="col-sm-12 col-md-12">
+    <div class="box box-solid box-primary ">
+      <!--  BOX HEADER      -->
+      <div class="box-header with-border">
+        <h3 class="box-title">Cargar Archivo de Informacion Laboral Formato CSV</h3>
+        <div class="box-tools pull-right">
+          <!--<button id="boton_minus_busqueda" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>-->
+        </div><!-- /.box-tools -->
+      </div><!-- /.box-header -->
+      
+      <!--  BOX-BODY  -->
+      <div class="box-body">
+      <!--CONTENIDO AQUI-->
+      <!--CONTENIDO AQUI-->
+      <!--CONTENIDO AQUI-->
+
+      <div class="row">
+      <br>
+        <div class="col-sm-2 col-md-2"></div>
+
+        <div class="col-sm-8 col-md-8">
+          
+          <form action='importar22.php' method='post' enctype="multipart/form-data">
+                
+                <div class="form-group row">
+                  <label class="control-label col-sm-3">Municipio:</label>
+                  <div class="col-xs-6">
+                    <select name="municipio" class="form-control" id="municipio">
+                      <option value="" disabled selected>Seleccione</option>
+                      <?php
+                      $sql=pg_query("select * from municipio");
+                      while ($cys_row = pg_fetch_array($sql)) {
+                      ?>
+                      <option value="<?php echo $cys_row['id_municipio']; ?>"><?php echo $cys_row['nombre_municipio']; ?></option>
+                      <?php } ?>
+                    </select>
+                  </div>
+                </div>
+
+                <div class="form-group row">
+                  <label class="control-label col-sm-3">Centro de Trabajo:</label>
+                  <div class="col-xs-6">
+                    <select name="municipio" class="form-control" id="municipio">
+                      <option value="" disabled selected>Seleccione</option>
+                      <?php
+                      $sql=pg_query("select * from municipio");
+                      while ($cys_row = pg_fetch_array($sql)) {
+                      ?>
+                      <option value="<?php echo $cys_row['id_municipio']; ?>"><?php echo $cys_row['nombre_municipio']; ?></option>
+                      <?php } ?>
+                    </select>
+                  </div>
+                </div>
+
+                <div class="form-group row">
+                  <label  class="control-label">  </label>
+                  <div class="controls">
+                    <input type='file' accept=".csv" title="Seleccione para Cargar el Archivo CSV" class="btn" name='sel_file' size='200000' >
+                  </div>
+                </div>
+
+                <div class="form-group">
+                  <input type='submit' class="btn btn-mini btn-primary" name='submit' value='Enviar'>
+                </div>
+
+              </form>
+            
+        </div>
+        <div class="col-sm-2 col-md-2"></div>
+      </div>
+
+
+        
+        
+
+
+
+      <!--./.....CONTENIDO AQUI-->
+      <!--./.....CONTENIDO AQUI-->
+      <!--./.....CONTENIDO AQUI-->
+                        
+      </div><!-- /.box-body -->
+    </div><!-- /.box -->    
+  </div><!-- /.col-sm-12 -->               
+</div>
+
+<!--...javascript AQUI-->
+<!--...javascript AQUI-->
+<!--...javascript AQUI-->
+<script src="apiv3.0/plugins/bootstrap-table/bootstrap-table.js"></script>
+<script src="apiv3.0/plugins/bootstrap-table/locale/bootstrap-table-es-SP.js"></script>
+
